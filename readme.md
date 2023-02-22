71d2f8accf40c0c4aa157ede8537f573f52337a0
dayuange <liu.zhiyuan@immomo.com>
2023-02-22 15:44:10 +0800

    readme

---
+++ readme.md
@@ -0,0 +1,11 @@
+## 使用方法
+![img.png](img.png)
+
+
+### 结果
+![img_1.png](img_1.png)
+
+
+
+### 依赖
+https://github.com/Grt1228/chatgpt-java
--- src/test/java/com/lzy/Test.java
+++ src/test/java/com/lzy/Test.java
@@ -1,2 +1,14 @@
-package com.lzy;public class Test {
+package com.lzy;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		LocalDateTime now = LocalDateTime.now();
+
+		long timestamp = now.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+	}
 }
